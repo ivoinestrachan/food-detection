3f2b881c51c0d22a041a4af013e5ced11d9b4a03
Ivoine <ivoinestrachan242@gmail.com>
2023-11-15 12:37:44 -0500

    testing model to detect basic fruits

---
+++ main.py
@@ -0,0 +1,16 @@
+import cv2
+import os
+from ultralytics import YOLO
+
+model = YOLO('yolov8n.pt')
+
+source = './ingredients'
+
+for filename in os.listdir(source):
+    if filename.lower().endswith(('.png', '.jpg', '.jpeg', 'webp')):
+        img_path = os.path.join(source, filename)
+
+        image = cv2.imread(img_path)
+        if image is not None:
+            results = model(image)
+
